b561fb25b14b2128b7e9ecdeeaa0864f5f25c6af
EvelynJimena <evelynjimenaez@gmail.com>
2023-09-27 00:19:25 -0500

    changes

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -9,12 +9,41 @@ public class Entry
     public void Display()
     {
         Console.WriteLine("Welcome to the Journal Program!");
+
         Console.WriteLine("Please select one of the following choices:");
 
-        foreach(PromptGenerator promptGenerator in _promptText)
+        Console.WriteLine("1. Write");
+        Console.WriteLine("2. Display");
+        Console.WriteLine("3. Load");
+        Console.WriteLine("4. Save");
+        Console.WriteLine("5. Quit");
+
+        Console.WriteLine("What would you like to do?");
+        string choices = Console.ReadLine();
+
+        if (choices == "1")
         {
-            promptGenerator.Display();
+        
+            Console.WriteLine("What are you grateful for right now?");
+            string answer =Console.ReadLine();
+            
         }
+
+        DateTime theCurrentTime = DateTime.Now;
+        string dateText = theCurrentTime.ToShortDateString();
+
+
+        string filename = "myFile.txt";
+        string[] lines = System.IO.File.ReadAllLines(filename);
+        foreach (string line in lines)
+        {
+        string[] parts = line.Split(",");
+
+        string firstName = parts[0];
+        string lastName = parts[1];
+        }
+
+
         
     
     }
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -9,10 +9,26 @@ class Program
         
         
         Entry anEntry = new Entry();
-        anEntry._promptText = "What reason made you smile today?";
+        anEntry._promptText = "What are you grateful for right now?";
         anEntry.Display();
         Console.WriteLine("Welcome to the Journal Program!"); 
         Console.WriteLine("Please select one of the following choices:");
+
+        Console.WriteLine("1. Write");
+        Console.WriteLine("2. Display");
+        Console.WriteLine("3. Load");
+        Console.WriteLine("4. Save");
+        Console.WriteLine("5. Quit");
+
+        Console.WriteLine("What would you like to do?");
+        string choices = Console.ReadLine();
+
+        if (choices == "1")
+        {
+        
+            Console.WriteLine("What are you grateful for right now?");
+            
+        }
 
         
         
