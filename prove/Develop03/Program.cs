70fa89d2a986837ec8225648a6e3aa866eceb5df
Danxark <dancaralonzo@gmail.com>
2023-09-30 23:40:17 -0500

    changes

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Console.WriteLine("Doctrine and Covenants 24: 8 Be patient in afflictions, for thou shalt have many; but endure them, for, lo, I am with thee, even unto the end of thy days.");
+
+        Console.WriteLine("Press enter to continue or type 'quit' to finish:");
+
+        Console.ReadLine();
+
+        Console.Clear();
+
+        Console.WriteLine("BBB");
     }
+
+    
 }
